5f8cad9253bb064574e7dbfebb2cd3b2c4589a25
Sanket4450 <talaviyasanket01@gmail.com>
2025-03-25 22:23:40 +0530

    initialized shadcn with tailwind css

--- app/root.tsx
+++ app/root.tsx
@@ -1,53 +1,36 @@
+import { LinksFunction } from '@remix-run/node'
 import {
-  Form,
   Links,
   Meta,
+  Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
+} from '@remix-run/react'
+
+import stylesheet from '~/tailwind.css?url'
+
+export const links: LinksFunction = () => [
+  { rel: 'stylesheet', href: stylesheet },
+]
 
 export default function App() {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        />
         <Meta />
         <Links />
       </head>
       <body>
-        <div id="sidebar">
-          <h1>Remix Contacts</h1>
-          <div>
-            <Form id="search-form" role="search">
-              <input
-                id="q"
-                aria-label="Search contacts"
-                placeholder="Search"
-                type="search"
-                name="q"
-              />
-              <div id="search-spinner" aria-hidden hidden={true} />
-            </Form>
-            <Form method="post">
-              <button type="submit">New</button>
-            </Form>
-          </div>
-          <nav>
-            <ul>
-              <li>
-                <a href={`/contacts/1`}>Your Name</a>
-              </li>
-              <li>
-                <a href={`/contacts/2`}>Your Friend</a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-
+        <h1 className="bg-red-500 text-white">Hello</h1>
+        <Outlet />
         <ScrollRestoration />
         <Scripts />
       </body>
     </html>
-  );
+  )
 }
